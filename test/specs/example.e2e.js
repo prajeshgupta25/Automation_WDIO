a665a7764e616a01fedba512866d28b841d6e68f
Prajesh Gupta <imprj.gupta@gmail.com>
2022-06-16 12:32:59 +0530

    Checkboxes and SelectBoxes

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -1,26 +1,26 @@
 describe('My First Test Suite', () => {
     it('my first wdio test', async () => {
         let myUrl = "https://www.google.com"
-    //     let smallPause = 2000
-    //     let bigPause = 5000
+        let smallPause = 2000
+        let bigPause = 5000
 
         //loading first url
 
-        // await browser.url(myUrl)
-        // await browser.pause(smallPause)
+        await browser.url(myUrl)
+        await browser.pause(smallPause)
         
         // MODERN WAY
 
-        // await expect(browser).toHaveTitleContaining('Google')
-        // await expect(browser).toHaveUrlContaining('google.com')
+        await expect(browser).toHaveTitleContaining('Google')
+        await expect(browser).toHaveUrlContaining('google.com')
 
-        // let pageElement = await $('a')
-        // await expect(pageElement).toExist()
-        // await expect(pageElement).toBeDisplayed()
-        // await expect(pageElement).toHaveTextContaining('Gmail')
+        let pageElement = await $('a')
+        await expect(pageElement).toExist()
+        await expect(pageElement).toBeDisplayed()
+        await expect(pageElement).toHaveTextContaining('Gmail')
     })
 
-    it.only('Forms and Inputs', async()=>{
+    it('Forms and Inputs', async()=>{
         await browser.url('https://www.saucedemo.com')
         let usernameInput = await $('#user-name')
         let passwordInput = await $('#password');
@@ -36,13 +36,19 @@ describe('My First Test Suite', () => {
         await loginBtn.click()
         await browser.pause(bigPause)
 
-        let inventoryContainer = await $('inventory_container')
+        let inventoryContainer = await $('#inventory_container')
         await expect(inventoryContainer).toBeDisplayed()
     })
 
+    it.only('SelectBox and CheckBoxe', async () =>{
+        await browser.url('devexpress.github.io/testcafe/example/')
+        
+        let selectbox = await $('#preferred-interface')
+        await selectbox.selectByVisibleText('Both')
 
-
-
+        let option = await $('option=Both')
+        await expect(option).toBeDisplayed()
+    })
 
 })
 
